fcf8086735bafe66d1a2909c290a7e354b7d70cd
Angiolo <113221737+AngioloOng@users.noreply.github.com>
2024-03-20 21:38:35 +0800

    Delete bakeshop_django-main/first directory

--- bakeshop_django-main/first/inventory/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('', views.home, name="dashboard"),
-    path('dashboard/', views.home, name="dashboard"),
-    path('inventory/', views.inventory, name='inventory'),
-    path('customer/', views.customer, name='customer'),
-]
