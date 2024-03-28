cc6ed19df81b90fba1e9fdf3574294d47ddbc5d9
Akira <124269519+AkiraAkiraxD@users.noreply.github.com>
2024-03-28 18:24:51 +0800

    Added pages to add and delete items in the database (not styled yet)

--- first/inventory/views.py
+++ first/inventory/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
+from .forms import itemForm
 
 # Create your views here.
 
@@ -14,3 +15,36 @@ def inventory(request):
 def customer(request):
 	return render(request, 'inventory/customer.html')
 
+def createItem(request):
+
+	form = itemForm()
+	if request.method == 'POST':
+		form = itemForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('/inventory')
+
+	context = {'form': form}
+	return render(request, 'inventory/item_form.html', context)
+
+def updateItem(request, pk):
+	item = inventoryItem.objects.get(id=pk)
+	form = itemForm(instance=item)
+
+	if request.method == 'POST':
+		form = itemForm(request.POST, instance=item)
+		if form.is_valid():
+			form.save()
+			return redirect('/inventory')
+
+	context = {'form': form}
+	return render(request, 'inventory/item_form.html', context)
+
+def deleteItem(request, pk):
+	item = inventoryItem.objects.get(id=pk)
+
+	if request.method == "POST":
+		item.delete()
+		return redirect('inventory')
+	context = {'item': item}
+	return render(request, 'inventory/delete.html', context)
