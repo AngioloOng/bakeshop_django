e94d81d39ec557e69ef55826d0a405ceb1cddd58
jimshi123 <123545282+jimshi123@users.noreply.github.com>
2024-04-04 17:50:21 +0800

    Add files via upload

--- first/customer/urls.py
+++ first/customer/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.customer_home, name='customer_home'),
-    path('customer_dashboard/', views.customer_dashboard, name='customer_dashboard'),
+    path('customer_dashboard', views.customer_dashboard, name='customer_dashboard'),
     path('view-products/', views.view_products, name='view_products'),
     path('login/', views.customer_login, name='customer_login'),
     path('signup/', views.customer_signup, name='customer_signup'),
@@ -15,11 +15,10 @@ urlpatterns = [
     
     
     path('messages/', views.support_messages, name='support_messages'),
-     path('messages/send/', views.send_message, name='send_message'),
+    path('messages/send/', views.send_message, name='send_message'),
     
     
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-     path('delivery-settings/', views.delivery_settings, name='delivery_settings'),
     path('set-delivery-options/', views.set_delivery_options, name='set_delivery_options'),
-    
+    path('view_users/', views.view_users, name='view_users'),
 ]
