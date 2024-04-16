f9bcf4d03a93b683fe636d63fd36e04e4411bb57
Angiolo <angiolo_ongkinglok@dlsu.edu.ph>
2024-04-16 13:00:10 +0800

    delivery tab additions
    addition per client's request

--- first/inventory/models.py
+++ first/inventory/models.py
@@ -48,6 +48,7 @@ class inventoryOrder(models.Model):
         default=NOT_PAID,
 		)
 	order = models.CharField(max_length = 60, null=True)
+	date_time = models.CharField(max_length = 60, null=True)
 
 	def __str__(self):
 		return self.name
